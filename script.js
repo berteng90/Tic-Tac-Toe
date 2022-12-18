c9972a50bd809231367ce94c0bd65c2848f92f05
GOODFARMS-ENG <berteng90@gmail.com>
2022-12-18 14:17:47 +0800

    Add simple AI Opponent

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ const gameBoard = [[EMPTY, EMPTY, EMPTY],
 [EMPTY, EMPTY, EMPTY],
 [EMPTY, EMPTY, EMPTY]]
 
+const winningCombinations = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
+  [2, 5, 8], [3, 6, 9], [1, 5, 9], [7, 5, 9]]
 
 const board = document.getElementById("game-container")
 
@@ -30,18 +33,42 @@ for (x = 0; x < gameBoard.length; x++) {
   for (y = 0; y < 3; y++) {
     const cell = document.createElement('div')
     cell.classList.add(`cell`)
+    cell.setAttribute('x-loc', x)
+    cell.setAttribute('y-loc', y)
     row.appendChild(cell)
     cell.addEventListener('click', () => {
-    })
-    const allCell = document.querySelectorAll(".cell")
-    allCell.forEach(() => {
+      let x = cell.getAttribute('x-loc')
+      let y = cell.getAttribute('y-loc')
+      if (!cell.textContent === EMPTY) {
 
+      } else {
+        console.log(player.marker)
+        cell.textContent = player.marker
+      }
+      updateBoard(x, y);
     })
+
   }
 }
 
 
 
-function updateBoard(cell) {
+function updateBoard(x, y) {
+  gameBoard[x][y] = 'X'
+  let foundEmptyCell = true
+  do {
+    const randomX = Math.floor(Math.random() * 3)
+    const randomY = Math.floor(Math.random() * 3)
+    if (gameBoard[randomX][randomY] === EMPTY) {
+      gameBoard[randomX][randomY] = 'O'
+      const cell = document.querySelector(`[x-loc="${randomX}"][y-loc="${randomY}"]`);
+      cell.textContent = 'O'
+      foundEmptyCell = true
+    } else {
+      foundEmptyCell = false
+    }
+    console.log(gameBoard)
+  }
+  while (foundEmptyCell = false)
 
 }
